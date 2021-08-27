e555011a5b3859bfd8f8feaaef5513fcefe5b03c
Thomas Ardal <thomasardal@gmail.com>
2021-08-27 10:31:21 +0200

    Added unit test with inner exception in stack trace

--- test/Elmah.Io.Client.Extensions.SourceCode.Test/SourceCodeFromPdbExtensionsTest.cs
+++ test/Elmah.Io.Client.Extensions.SourceCode.Test/SourceCodeFromPdbExtensionsTest.cs
@@ -20,6 +20,39 @@ namespace Elmah.Io.Client.Extensions.SourceCode.Test
     at System.Web.HttpApplication.MapHttpHandler(HttpContext context, String requestType, VirtualPath path, String pathTranslated, Boolean useAppConfig)
     at System.Web.HttpApplication.MapHandlerExecutionStep.System.Web.HttpApplication.IExecutionStep.Execute()
     at System.Web.HttpApplication.ExecuteStep(IExecutionStep step, Boolean& completedSynchronously)";
+        internal const string DotNetInnerExceptionStackTrace = @"System.SystemException: Oh no from x
+ ---> System.ApplicationException: Oh no from y
+ ---> System.DivideByZeroException: Attempted to divide by zero.
+   at ConsoleApp53.B.Y()
+   --- End of inner exception stack trace ---
+   at ConsoleApp53.B.Y()
+   at ConsoleApp53.A.X()
+   --- End of inner exception stack trace ---
+   at Elmah.Io.Client.Extensions.SourceCode.Test.SourceCodeFromPdbExtensionsTest.CanDecorateMessageWithCode() in REPLACE_ME\SourceCodeFromPdbExtensionsTest.cs:line 36
+   at ConsoleApp53.Program.Main(String[] args) in C:\Users\thoma\source\repos\ConsoleApp53\ConsoleApp53\Program.cs:line 11";
+
+        [Test]
+        public void CanDecorateMessageWithInnerExceptionWithCode()
+        {
+            // Arrange
+            var path = new FileInfo(Assembly.GetExecutingAssembly().Location);
+            var root = path.Directory.Parent.Parent.Parent;
+            var msg = new CreateMessage
+            {
+                Detail = DotNetInnerExceptionStackTrace.Replace("REPLACE_ME", root.FullName)
+            };
+
+            // Act
+            msg = msg.WithSourceCodeFromPdb();
+
+            // Assert
+            Assert.That(!string.IsNullOrWhiteSpace(msg.Code));
+            Assert.That(msg.Code.Contains("REPLACE_ME\\SourceCodeFromPdbExtensionsTest.cs:line 36"));
+            Assert.That(msg.Data != null);
+            Assert.That(msg.Data.Count, Is.EqualTo(2));
+            Assert.That(msg.Data.First(d => d.Key == "X-ELMAHIO-CODESTARTLINE").Value, Is.EqualTo("26"));
+            Assert.That(msg.Data.First(d => d.Key == "X-ELMAHIO-CODELINE").Value, Is.EqualTo("36"));
+        }
 
         [Test]
         public void CanDecorateMessageWithCode()
@@ -37,7 +70,7 @@ namespace Elmah.Io.Client.Extensions.SourceCode.Test
 
             // Assert
             Assert.That(!string.IsNullOrWhiteSpace(msg.Code));
-            Assert.That(msg.Code.Contains("Detail = DotNetStackTrace.Replace(\"REPLACE_ME\", root.FullName)"));
+            Assert.That(msg.Code.Contains("REPLACE_ME\\SourceCodeFromPdbExtensionsTest.cs:line 36"));
             Assert.That(msg.Data != null);
             Assert.That(msg.Data.Count, Is.EqualTo(2));
             Assert.That(msg.Data.First(d => d.Key == "X-ELMAHIO-CODESTARTLINE").Value, Is.EqualTo("26"));
